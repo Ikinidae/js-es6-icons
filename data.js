93828f4ccf779aa3bf6685e120c6af1148bdba5d
Ikinidae <federicaelia24@gmail.com>
2022-05-19 17:35:44 +0200

    Progetto iniziato

---
+++ data.js
@@ -0,0 +1,150 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+console.log(icons);
+
+// bisogna creare un div da inserire nel container per ogni icona
+// al suo interno bisogna inserire l'icona e un h3 per il nome
+
+const container = document.querySelector(".icons_container");
+console.log(container);
+
+// let divIcon = document.createElement("div");
+// container.append(divIcon);
+// let icon = document.createElement("i");
+// icon.classList.add("fas", "fa-cat");
+// divIcon.append(icon);
+// let iconTag = document.createElement("h3");
+// divIcon.append(iconTag);
+// let iconText = document.createTextNode(icons[0].name);
+// iconTag.append(iconText);
+
+
+for (let i=0; i < icons.length; i++) {
+	let divIcon = document.createElement("div");
+	container.append(divIcon);
+
+	let icon = document.createElement("i");
+	icon.classList.add(icons[i].family, icons[i].prefix + icons[i].name);
+	divIcon.append(icon);
+
+	let iconTag = document.createElement("h3");
+	divIcon.append(iconTag);
+	let iconText = document.createTextNode(icons[i].name);
+	iconTag.append(iconText);
+}
+
+// console.log(container);
+
